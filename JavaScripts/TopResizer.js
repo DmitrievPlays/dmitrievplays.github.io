ca533f19b5b459af803a6ca45d2f3d55f279f92f
DmitrievPlays <dmitriev8925@gmail.com>
2020-08-06 13:52:26 +0300

    Update.

--- JavaScripts/TopResizer.js
+++ JavaScripts/TopResizer.js
@@ -2,6 +2,7 @@ function net(){
 var intElemOffsetHeight = document.querySelector(".ThePageTitle").offsetHeight;
 
 document.querySelector(".Content").style.marginTop = document.querySelector(".ThePageTitle").offsetHeight + "px";
+console.log(document.querySelector(".ThePageTitle").offsetHeight + "px");
 
 if (document.querySelector(".White") == undefined && window.innerWidth < 460)
 	document.querySelector("body").innerHTML += '<div class="White" style="position: relative; width: 100%; height: 100vh; background: #bbb; z-index: 1000;"><div style="position: fixed; top: 50%; display: block; font-size: calc(2vw + 2vh); white-space: normal;">СЛИШКОМ НИЗКОЕ РАЗРЕШЕНИЕ</div></div>';
