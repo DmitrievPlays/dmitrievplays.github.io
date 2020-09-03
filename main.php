a15607cb4c1cf8c92d93226500dfd69c3f6cdf69
DmitrievPlays <dmitriev8925@gmail.com>
2020-09-03 13:02:27 +0300

    3

--- main.php
+++ main.php
@@ -2,10 +2,18 @@
  <head>
   <title>Тестируем PHP</title>
   <link rel="stylesheet" type="text/css" href="style.css">
+  <style type = "text/css">
+    #menu
+    {
+      background: red;
+      width: 100px;
+      height: 200px;
+    }
+  </style>
  </head>
  <body>
  <div id = "menu">
-        <a href = "site.php">Привет, мир!</a>
+        <a href = "main.php">Привет, мир!</a>
         <a href = "f">Привет, мир!</a>
         <a href = "f">Привет, мир!</a>
         <a href = "f">Привет, мир!</a>
