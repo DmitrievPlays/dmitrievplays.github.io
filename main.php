f0b6e2794184cb676e86598c65d8747f39bc0696
DmitrievPlays <dmitriev8925@gmail.com>
2020-09-03 12:54:28 +0300

    1

---
+++ main.php
@@ -0,0 +1,14 @@
+<html>
+ <head>
+  <title>Тестируем PHP</title>
+  <link rel="stylesheet" type="text/css" href="style.css">
+ </head>
+ <body>
+ <div class = "menu">Привет, мир!
+        <a href = "site.php">Привет, мир!</div>
+        <a href = "f">Привет, мир!</div>
+        <a href = "f">Привет, мир!</div>
+        <a href = "f">Привет, мир!</div>
+</div>
+ </body>
+</html>
